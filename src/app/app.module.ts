aa5e7ee97b138d9f9a0a6fe399a8119a45a769c8
FelipeEspinoza <felipe.al377@gmail.com>
2022-12-01 00:22:42 -0300

    feat: rutas hijas y lazy load

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+// Modulos
+import { AppRoutingModule } from './app-routing.module';
+
+// Componentes
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +18,8 @@ import { ErrorPageComponent } from './shared/error-page/error-page.component';
   ],
   imports: [
     BrowserModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
